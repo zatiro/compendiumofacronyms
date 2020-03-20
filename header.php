ec9675c3533b7422d9f1943d6a42524acd279e2d
zatiro <40040933+zatiro@users.noreply.github.com>
2020-03-20 16:47:34 -0300

    Add bootstrap and header

---
+++ header.php
@@ -0,0 +1,15 @@
+<header>
+    <div class="container">
+        <div class="row">
+
+            <div class="col header-col-1"></div>
+
+            <div class="col-md-auto header-col-2">
+                Compendium of Acronyms
+            </div>
+
+            <div class="col header-col-3"></div>
+
+        </div>
+    </div>
+</header>
