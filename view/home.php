c197a6987487bb636ae7e7c2d83c9e8640a36f45
zatiro <40040933+zatiro@users.noreply.github.com>
2020-03-20 18:07:07 -0300

    Ajustments

--- view/home.php
+++ view/home.php
@@ -1,5 +1,11 @@
+<?php
+
+    $query = "select * from compendium;"
+
+?>
+
 <div class="card bg-light">
-  <div class="card-header">Header</div>
+  <h4 class="card-header"><span>CofA</span> - Compendium of Acronyms</h4>
   <div class="card-body">
     <h5 class="card-title">Light card title</h5>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
