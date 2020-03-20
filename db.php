53ec50c2b45934f0b8d22c5962e77a0e854e9b2d
zatiro <40040933+zatiro@users.noreply.github.com>
2020-03-20 16:34:29 -0300

    Create db, head scripts and styles

---
+++ db.php
@@ -0,0 +1,17 @@
+<?php
+
+# conexão com o banco de dados
+$servidor = 'us-cdbr-iron-east-04.cleardb.net';
+$usuario = 'b67e91c0dec3ac';
+$senha = '2f61ddaa';
+$database = 'heroku_73bd3f8c7d8b75c';
+
+$conexao = mysqli_connect($servidor, $usuario, $senha, $database);
+
+if(!$conexao){
+    echo 'Erro ao conectar com o banco de dados';
+}
+else{
+    echo 'Conectado ao banco de dados com sucesso!';
+}
+?>
--- index.php
+++ index.php
@@ -1,5 +1,22 @@
 <?php
 
-echo "Saporra!";
+    include_once('db.php');
 
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>CofA - Compendium Of Acronyms</title>
+    <meta charset="UTF-8">
+
+    <?php 
+        include_once('scriptsHead.php');
+        include_once('stylesHead.php');
+    ?>
+
+</head>
+
+<body>
+    
+</body>
