945c94c6aa7f8e8b54dc21f0a50b21a69ccc5070
zatiro <40040933+zatiro@users.noreply.github.com>
2020-03-20 21:59:17 -0300

    Searching insert error

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
             <div class="col header-col-1"></div>
 
             <div class="col header-col-2">
-                <h3 style="color: white;">Compendium of Acronyms</h3>
+                <a href="index.php"><h3 style="color: white;">Compendium of Acronyms</h3></a>
             </div>
 
             <div class="col header-col-3"></div>
