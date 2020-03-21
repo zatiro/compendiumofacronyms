70fc5fc8cdbfba0b9320ce1567ea85419dd5edaf
zatiro <40040933+zatiro@users.noreply.github.com>
2020-03-20 21:56:30 -0300

    name="..."

--- view/insertAcronym.php
+++ view/insertAcronym.php
@@ -7,12 +7,12 @@
 
                 <div class="col">
                     <label for="formGroupExampleInput">The acronym</label>
-                    <input type="text" class="form-control" id="acronym" placeholder="Ex: PN" required>
+                    <input type="text" class="form-control" id="acronym" name="acronym" placeholder="Ex: PN" required>
                 </div>
 
                 <div class="col">
                     <label for="description">means</label>
-                    <textarea class="form-control" id="description" rows="2" required></textarea>
+                    <textarea class="form-control" id="description" name="description" rows="2" required></textarea>
                 </div>
 
             </div>
