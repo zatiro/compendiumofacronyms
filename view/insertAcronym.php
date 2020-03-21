919694789d180136ebfc9a6ae440e7d3a0bffef0
zatiro <40040933+zatiro@users.noreply.github.com>
2020-03-20 21:47:09 -0300

    Update insertAcronyms .php

--- view/insertAcronym.php
+++ view/insertAcronym.php
@@ -3,21 +3,21 @@
     <div class="card-body">
         <h5 class="card-title">Special title treatment</h5>
 
-        <form method="post" action="insertAcronymProcess.php">
+        <form method="post" action="view/insertAcronymProcess.php">
             <div class="row">
 
-                <div class="col-md-auto">
+                <div class="col">
                     <label for="formGroupExampleInput">The acronym</label>
                     <input type="text" class="form-control" id="acronym" placeholder="Ex: PN" required>
                 </div>
 
-                <div class="col-md-auto">
+                <div class="col">
                     <label for="description">means</label>
                     <textarea class="form-control" id="description" rows="2" required></textarea>
                 </div>
 
             </div>
-            <button type="submit" class="btn btn-success">Insert</button>
+            <button type="submit" class="btn btn-success" placeholder="Ex: Part Number ou Número da Peça">Insert</button>
         </form>
         <?php
             if(isset($_GET['result'])){
